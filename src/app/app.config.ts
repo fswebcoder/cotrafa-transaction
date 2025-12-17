cea3a21b59b0ffa97c932e939d2ef2792e4c8719
Fabio Sánchez <fabio.ssanchez92@gmail.com>
2025-12-16 20:40:54 -0500

    Creación del proyecto y configuración del tema en el appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,46 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeuix/themes';
+import Nora from '@primeng/themes/nora';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+const cfcPreset = definePreset(Nora, {
+    semantic: {
+        primary: {
+            50: '#ecf4ff',
+            100: '#d5e6ff',
+            200: '#b2d3ff',
+            300: '#80b6ff',
+            400: '#468fff',
+            500: '#004eb5',
+            600: '#003e91',
+            700: '#002f6d',
+            800: '#002049',
+            900: '#001025',
+            950: '#000812'
+        },
+    }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: cfcPreset,
+        options: {
+          darkModeSelector: '.app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind-base, primeng, tailwind-utilities'
+          }
+        }
+      }
+    }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+
   ]
 };
